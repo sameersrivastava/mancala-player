441da60a04519771233d2ed8a0c47b01504aef4f
sameersrivastava <sameer.srivastava95@gmail.com>
2015-12-12 00:09:25 -0500

    Got all the cups and mancala to be drawn in drawBoard. Works well and scales properly.

--- js/mancala.js
+++ js/mancala.js
@@ -9,15 +9,15 @@ var mancala = (function($){
 		document.body.appendChild(canvas);
 		//Set drawing units and players
 		var canvas_unit = canvas.height / 2,
-			cup_radius = canvas.height * 3 / 16,
+			cup_radius = canvas_unit * 3 / 8,
 			p1 = 1,
 			p2 = 2,
 			turn = p1,
 			wait = p2,
-			p1_cups = new Array(),
-			p2_cups = new Array(),
-			p1_mancala,
-			p2_mancala;
+			p1_draw_cups = new Array(),
+			p2_draw_cups = new Array(),
+			p1_draw_mancala,
+			p2_draw_mancala;
 
 		//makeBoard data objects
 		makeBoard();
@@ -32,9 +32,9 @@ var mancala = (function($){
 			w_increment = canvas_unit;
 			h_offset = (canvas_unit / 2);
 			h_increment = canvas_unit;
-			//P1 Cups Bottom
+			//P2 Cups Top
 			for(i=0; i < 6; i += 1) { 
-				p2_cups[i] = {
+				p2_draw_cups[i] = {
 					x: w_offset + i * w_increment,h_offset,
 					y: h_offset,
 					radius: cup_radius,
@@ -45,8 +45,9 @@ var mancala = (function($){
 					isHovering: false
 				}
 			}
+			//P1 Cups Bottom
 			for(i=0; i < 6; i += 1) { 
-				p1_cups[i] = {
+				p1_draw_cups[i] = {
 					x: w_offset + i * w_increment,h_offset,
 					y: h_offset + h_increment,
 					radius: cup_radius,
@@ -57,26 +58,108 @@ var mancala = (function($){
 					isHovering: false
 				}
 			}
+			//P2 Mancala
+			p2_draw_mancala = {
+				x: canvas_unit / 2,
+				y: canvas_unit,
+				radius: cup_radius,
+				points: [
+					{x: canvas_unit / 2, y: canvas_unit / 2},
+					{x: canvas_unit / 2 + cup_radius, y: canvas_unit * 3 / 2},
+					{x: canvas_unit / 2, y: canvas_unit * 3 / 2},
+					{x: canvas_unit / 8, y: canvas_unit / 2}
+				]
+			}
+
+
+
+			//P1 Mancala
+			p1_draw_mancala = {
+				x: canvas_unit * 7 / 2,
+				y: canvas_unit,
+				radius: cup_radius,
+				points: [
+					{x: canvas_unit * 15 / 2, y: canvas_unit / 2},
+					{x: canvas_unit * 15 / 2 + cup_radius, y: canvas_unit * 3 / 2},
+					{x: canvas_unit * 15 / 2, y: canvas_unit * 3 / 2},
+					{x: canvas_unit * 57 / 8, y: canvas_unit / 2}
+				]
+			}
 		}
 		function drawBoard() {
 			var i, cup;
 			//Draw cups
 			for(i = 0; i < 6; i += 1){
 				//Draw P1 Cups
-				cup = p1_cups[i];
+				cup = p1_draw_cups[i];
 				ctx.beginPath();				
 				ctx.arc(cup.x,cup.y,cup.radius*0.9,0,2 * Math.PI);
 				ctx.strokeStyle = "black";
 				ctx.lineWidth = cup.radius * 0.1;
 				ctx.stroke();
 				//Draw P2 cups
-				cup = p2_cups[i];
+				cup = p2_draw_cups[i];
 				ctx.beginPath();				
 				ctx.arc(cup.x,cup.y,cup.radius*0.9,0,2 * Math.PI);
 				ctx.strokeStyle = "black";
 				ctx.lineWidth = cup.radius * 0.1;
 				ctx.stroke();	
 			}
+			//Draw P2 Mancala
+			ctx.beginPath();
+			ctx.arc(
+				p2_draw_mancala.points[0].x,
+				p2_draw_mancala.points[0].y,
+				p2_draw_mancala.radius,
+				Math.PI,
+				2*Math.PI
+			);
+			ctx.lineTo(
+				p2_draw_mancala.points[1].x,
+				p2_draw_mancala.points[1].y
+			);
+			ctx.arc(
+				p2_draw_mancala.points[2].x,
+				p2_draw_mancala.points[2].y,
+				p2_draw_mancala.radius,
+				0,
+				Math.PI
+			);
+			ctx.lineTo(
+				p2_draw_mancala.points[3].x,
+				p2_draw_mancala.points[3].y
+			);
+			ctx.strokeStyle = "black";
+			ctx.lineWidth = cup.radius * 0.1;
+			ctx.stroke();
+
+			//P1 Mancala
+			ctx.beginPath();
+			ctx.arc(
+				p1_draw_mancala.points[0].x,
+				p1_draw_mancala.points[0].y,
+				p2_draw_mancala.radius,
+				Math.PI,
+				2*Math.PI
+			);
+			ctx.lineTo(
+				p1_draw_mancala.points[1].x,
+				p1_draw_mancala.points[1].y
+			);
+			ctx.arc(
+				p1_draw_mancala.points[2].x,
+				p1_draw_mancala.points[2].y,
+				p2_draw_mancala.radius,
+				0,
+				Math.PI
+			);
+			ctx.lineTo(
+				p1_draw_mancala.points[3].x,
+				p1_draw_mancala.points[3].y
+			);
+			ctx.strokeStyle = "black";
+			ctx.lineWidth = cup.radius * 0.1;
+			ctx.stroke();
 		}
 
 
