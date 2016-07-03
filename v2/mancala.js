c5838086e150d9d3274e1c9bb462023f9770ecae
sameersrivastava <sameer.srivastava95@gmail.com>
2016-07-03 15:33:46 -0400

    Adding some code right now before moving on.

--- v2/mancala.js
+++ v2/mancala.js
@@ -21,11 +21,80 @@ Program Overview:
 	-Switch turns if not Move Again
 	-Put in new values in cups
 	-Repeat until Game over
+
+
+3 classes
+-Mancala Window => GUI
+-Mancala Board => "Backend"
+-Player => Uses Backend
 */
 var mancala = (function($){
 	var main = function(){
 		console.log('test');
+		var gui = new mancala_gui(2, 3);
+		gui.drawBoard();
 	};
+	//Front end markup
+	class mancala_gui {
+		constructor(p1, p2){
+			this.game = new mancala_board();
+			this.p1 = p1;
+			this.p2 = p2;
+		}
+		drawBoard(){
+			$('.pit.player-1').each(function(index){
+				console.log( index + ": " + $( this ).text() );
+			});
+			$($('.pit.player-2').get().reverse()).each(function(index){
+				console.log( index + ": " + $( this ).text() );
+			});
+
+
+			//run continueGame
+			//this.continueGame();
+		}
+		newgame(){
+			this.game.reset();
+			this.turn = this.p1; //change this to random later
+			this.wait = this.p2;
+			this.drawBoard();
+			
+		}
+		continueGame(){
+			if (this.game.gameOver()) {
+            	if (this.game.hasWon(this.p1.num)){
+                	this.status = "Player " + str(this.p1) + " wins";
+            	} else if (this.game.hasWon(this.p2.num)){
+                	this.status = "Player " + str(this.p2) + " wins";
+            	} else
+                	this.status = "Tie game";
+            	return
+            }
+        	if (this.turn.type == player.HUMAN) {
+            	this.enableBoard();
+        	} else {
+            	move = this.turn.chooseMove(this.game);
+            	playAgain = this.game.makeMove(this.turn, move);
+            	if (!playAgain) {
+                	this.swapTurns()
+            	}
+           		this.drawBoard();
+           	}
+		}
+	}
+	//Game Logic
+	class mancala_board{
+		constructor(){
+			this.reset();
+		}
+		reset(){
+			this.num_cups = 6;
+			this.score_cups = [0,0];
+			this.p1_cups = Array.apply(null, Array(this.num_cups)).map(Number.prototype.valueOf,4);
+			this.p2_cups = Array.apply(null, Array(this.num_cups)).map(Number.prototype.valueOf,4);
+		}
+
+	}
 	return {
 		main: function() {
 			return main();
