097ab02b2cb20a625f3bc209e97bf76671eba827
sameersrivastava <sameer.srivastava95@gmail.com>
2016-07-05 22:03:47 -0400

    Got it to work. Need to figure out callbacks, but its ok for now.

--- v2/mancala.js
+++ v2/mancala.js
@@ -32,9 +32,42 @@ var mancala = (function($){
 	var main = function(){
 		
 		var player1 = new player(1, 0),
-			player2 = new player(2, 1),
-			gui = new mancala_gui(player1, player2);
-		gui.newgame();
+			player2 = new player(2, 2, 9),
+			gui;
+			// player2 = new player(2, 1),
+			
+		$('.btn-success').click(function(){
+			$('.chat-bubble').html(
+				'Who goes first?' +
+				'<button type="button" class="btn btn-default" id="btn-p1">P1 (You) </button>' +
+				'<button type="button" class="btn btn-default" id="btn-p2">P2 (Me) </button>' +
+				'<button type="button" class="btn btn-default" id="btn-random">Random</button>'
+			);
+			$('#btn-p1').click(function(){
+				gui = new mancala_gui(player1, player2);
+				$('.chat-bubble').text('You can start!');
+				window.setTimeout(gui.newgame(), 2000);
+			});
+			$('#btn-p2').click(function(){
+				gui = new mancala_gui(player2, player1);
+				$('.chat-bubble').text('I will start!');
+				window.setTimeout(gui.newgame(), 2000);
+			});
+			$('#btn-random').click(function(){
+				if(Math.random() >= 0.5) {
+					gui = new mancala_gui(player1, player2);
+					$('.chat-bubble').text('You can start!');
+					window.setTimeout(gui.newgame(), 2000);
+				} else {
+					gui = new mancala_gui(player2, player1);
+					$('.chat-bubble').text('I will start!');
+					window.setTimeout(gui.newgame(), 2000);
+				}
+			});
+
+			// window.setTimeout(gui.newgame(), 2000);
+		});
+		
 	};
 	//Front end markup
 	class mancala_gui {
@@ -72,10 +105,10 @@ var mancala = (function($){
 			if (this.game.gameOver()) {
             	if (this.game.hasWon(this.p1.num)){
                 	// this.status = "Player " + this.p1 + " wins";
-                	$('.chat-bubble').text('Player 1 wins!');
+                	$('.chat-bubble').text('Player ' + this.p1.num +' wins!');
             	} else if (this.game.hasWon(this.p2.num)){
                 	//this.status = "Player " + this.p2 + " wins";
-                	$('.chat-bubble').text('Player 2 wins!');
+                	$('.chat-bubble').text('Player ' + this.p2.num +' wins!');
             	} else
                 	// this.status = "Tie game";
                 	$('.chat-bubble').text('It is a tie!');
@@ -88,6 +121,7 @@ var mancala = (function($){
             	playAgain = this.game.makeMove(this.turn, move);
             	if (!playAgain) {
                 	// this.swapTurns();
+                	console.log('Turn swap');
                 	var temp = this.turn
         			this.turn = this.wait
         			this.wait = temp
@@ -122,8 +156,18 @@ var mancala = (function($){
 	}
 	//Game Logic
 	class mancala_board{
-		constructor(){
-			this.reset();
+		// constructor(){
+		// 	this.reset();
+		// }
+		constructor(score_cups, p1_cups, p2_cups){
+			if(score_cups === undefined){
+				this.reset();
+			} else {
+				this.num_cups = 6;
+				this.score_cups = score_cups;
+				this.p1_cups = p1_cups;
+				this.p2_cups = p2_cups;
+			}
 		}
 		reset(){
 			this.num_cups = 6;
@@ -160,7 +204,7 @@ var mancala = (function($){
 			return moves;
 		}
 		//make the move
-		makeMove(player, cup, makeMoveCallback){
+		makeMove(player, cup){
 			return this.makeMoveHelp(player, cup);
 			
 		}
@@ -288,14 +332,269 @@ var mancala = (function($){
 			this.ply = ply;
 		}
 		chooseMove(board) {
-			var move, legal_moves;
+			var move, legal_moves, val;
 			if (this.type == 1) {
 				legal_moves = board.legalMoves(this);
 				move = legal_moves[Math.floor(Math.random()*legal_moves.length)];
 				return move;
+			} else if(this.type == 2){
+				val = this.alphaBetaMove(board, this.ply);
+				console.log('chose move ' + val[1], ' with value' + val[0]);
+				return val[1];
 			}
 		}
+		//=========Min Max Functions=========
+		minimaxMove(board,ply){
+			var move = -1,
+				score = -5000.0,
+				turn = this,
+				legal_moves = board.legalMoves(this),
+				i, nb, opp, s;
+			for(i = 0; i < legal_moves.length; i += 1){
+				if (ply == 0){
+					return [this.score(board), legal_moves[i]];
+				}
+				if (board.gameOver()){
+					return [-1, -1]; //no moves possible
+				}
+				//deep copy the board
+				nb = new mancala_board(
+					board.score_cups.slice(),
+					board.p1_cups.slice(),
+					board.p2_cups.slice()
+				);
+				//try the move
+				nb.makeMove(this, legal_moves[i]);
 
+				opp = new player(this.opp, this.type, this.ply);
+				s = opp.minValue(nb, ply - 1, turn);
+
+				if (s > score){
+					move = legal_moves[i];
+					score = s;
+				}
+			}
+			return [score, move];
+		}
+		maxValue(board, ply, turn){
+			var score = -5000.0,
+				legal_moves = board.legalMoves(this),
+				opp, nb, s;
+			if (board.gameOver()){
+				return turn.score(board);
+			}
+			for(i = 0; i < legal_moves.length; i += 1){
+				if (ply == 0){
+					return turn.score(board);
+				}
+				opp = new player(this.opp, this.type, this.ply);
+				nb = new mancala_board(
+					board.score_cups.slice(),
+					board.p1_cups.slice(),
+					board.p2_cups.slice()
+				);
+				//try the move
+				nb.makeMove(this, legal_moves[i]);
+				s = opp.minValue(nb, ply - 1, turn);
+				if (s > score){
+					score = s;
+				}
+			}
+			return score;
+		}
+		minValue(board, ply, turn){
+			var score = 5000.0,
+				legal_moves = board.legalMoves(this),
+				opp, nb, s;
+			if (board.gameOver()){
+				return turn.score(board);
+			}
+			for(i = 0; i < legal_moves.length; i += 1){
+				if (ply == 0){
+					return turn.score(board);
+				}
+				opp = new player(this.opp, this.type, this.ply);
+				nb = new mancala_board(
+					board.score_cups.slice(),
+					board.p1_cups.slice(),
+					board.p2_cups.slice()
+				);
+				//try the move
+				nb.makeMove(this, legal_moves[i]);
+				s = opp.maxValue(nb, ply - 1, turn);
+				if (s < score){
+					score = s;
+				}
+			}
+			return score;
+		}
+		//=========Alpha Beta Functions=========
+		alphaBetaMove(board, ply){
+			var alpha = -5000.0,
+				beta = 5000.0;
+			return this.alphaMaxValue(board, ply, this, alpha, beta);
+		}
+		alphaMaxValue(board, ply, turn, alpha, beta){
+			var score = -5000.0,
+				move = -1,
+				legal_moves = board.legalMoves(this),
+				i, opp, nb, val, s, mv, again;
+			if ((board.gameOver()) || (ply==0)){
+				return [turn.score(board), -1]
+			}
+			for(i = 0; i < legal_moves.length; i += 1){
+				
+				opp = new player(this.opp, this.type, this.ply);
+				nb = new mancala_board(
+					board.score_cups.slice(),
+					board.p1_cups.slice(),
+					board.p2_cups.slice()
+				);
+				//try the move
+				again = nb.makeMove(this, legal_moves[i]);
+				if (again){
+					val = opp.alphaMaxValue(nb, ply - 1, turn, alpha, beta);
+				} else {
+					val = opp.alphaMinValue(nb, ply - 1, turn, alpha, beta);
+				}
+				s  = val[0];
+				mv = val[1];
+				if (s > score){
+					score = s;
+					move = legal_moves[i];
+				}
+
+				if (score >= beta){
+					return [score, move];
+				}
+				if (score > alpha){
+					alpha = score;
+				}
+			}
+			return [score, move];
+		}
+		alphaMinValue(board, ply, turn, alpha, beta){
+			var score = 5000.0,
+				move = -1,
+				legal_moves = board.legalMoves(this),
+				i, opp, nb, val, s, mv, again;
+			if ((board.gameOver()) || (ply==0)){
+				return [turn.score(board), -1]
+			}
+			for(i = 0; i < legal_moves.length; i += 1){
+				
+				opp = new player(this.opp, this.type, this.ply);
+				nb = new mancala_board(
+					board.score_cups.slice(),
+					board.p1_cups.slice(),
+					board.p2_cups.slice()
+				);
+				//try the move
+				again = nb.makeMove(this, legal_moves[i]);
+				if (again){
+					val = opp.alphaMinValue(nb, ply - 1, turn, alpha, beta);
+				} else {
+					val = opp.alphaMaxValue(nb, ply - 1, turn, alpha, beta);
+				}
+				// val = opp.alphaMaxValue(nb, ply - 1, turn, alpha, beta);
+				s  = val[0];
+				mv = val[1];
+				if (s < score){
+					score = s;
+					move = legal_moves[i];
+				}
+
+				if (score <= alpha){
+					return [score, move];
+				}
+				if (score < beta){
+					beta = score;
+				}
+			}
+			return [score, move];
+		}
+		//Score Function
+		score(board){
+			var cups, opp_cups,
+				score_diff, score,
+				additional,
+				capturing,
+				marbles, num_marbles,
+				i, temp, temp_capturing,
+				own_marbles,
+				total;
+
+			if (board.hasWon(this.num)){
+				return 1000.0;
+			} else if(board.hasWon(this.opp)){
+				return -1000.0;
+			} else if (board.score_cups[this.num - 1] > 24){
+				return 500.0;
+			} else if (board.score_cups[this.opp - 1] > 24){
+				return -500.0;
+			} else {
+				if(this.num == 1){
+					cups = board.p1_cups;
+					opp_cups = board.p2_cups;
+					score_diff = board.score_cups[0] - board.score_cups[1];
+				} else {
+					cups = board.p2_cups;
+					opp_cups = board.p1_cups;
+					score_diff = board.score_cups[1] - board.score_cups[0];
+				}
+
+				score = score_diff + 24 * 30 / 6;
+
+				additional = 0.0;
+				capturing = 0.0;
+				marbles = 0.0;
+
+				for(i = 0; i < cups.length; i += 1){
+					num_marbles = cups[i];
+					if ((num_marbles > 0) || (opp_cups[i] > 0)) {
+						if (num_marbles == (6- i)) {
+							additional = (50.0 * (i + 1)) / 6;
+						}
+						//capturing
+						temp = opp_cups[i] % 13;
+						temp_capturing = 0.0;
+						own_marbles = -1;
+						if(temp <= (5 - i)){
+							if (temp == 0){
+								own_marbles = cups[5 - i];
+							} else {
+								own_marbles = cups[5 - i - temp];
+							}
+						} else if (temp >= (13- i)){
+							own_marbles = cups[5 - i + temp - 13];
+						}
+						if (own_marbles == 0){
+							temp_capturing = 50.0;
+						} else if (own_marbles == 1){
+							temp_capturing = 40.0;
+						} else if (own_marbles == 2){
+							temp_capturing = 25.0;
+						}
+
+						if (temp_capturing > capturing) {
+							capturing = temp_capturing;
+						}
+						if (num_marbles < 4){
+							marbles += 5;
+						}
+					} else {
+						if (i == 5){
+							marbles += 30;
+						} else {
+							marbles += 10;
+						}
+
+					}
+				}
+				total = capturing + score + additional + marbles;
+				return total;
+			}
+		}
 	}
 	return {
 		main: function() {
